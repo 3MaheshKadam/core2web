b2ed1078d6c5e5090066dd146f43e5f6434f286f
3MaheshKadam <86357696+3MaheshKadam@users.noreply.github.com>
2024-01-23 22:21:26 +0530

    PDF Reader using python

--- Assignments/assignment6.py
+++ Assignments/assignment6.py
@@ -1,10 +1,69 @@
-# # while loops
-# i=1
-# x =int(input("Enter a numbaer"))
-# while(i<x):
-#     if(i%2!=0):
-#         print(i)
-#     i+=1
-# print("terminated")
+# # # while loops
+# # i=1
+# # x =int(input("Enter a numbaer"))
+# # while(i<x):
+# #     if(i%2!=0):
+# #         print(i)
+# #     i+=1
+# # print("terminated")
 
-player =['rohit','gill' ]
+# player =['rohit','yuvi',"gauti","rahane","ambati","shikhar" ]
+# playerName = input("Enter a player name to check in team :")
+# count =0
+# for i in player:
+#     if playerName == i:
+#         print(playerName,"is in the 11")
+#         break
+#     count+=1
+# print(count)
+
+# code 1 reverse nos from 10
+
+# x = 10
+# while 11 > x:
+#     print(x)
+#     x-=1
+#     if x==0:
+#         break   
+
+'''
+sum=0    #sum of all even numbers ranging from 1 to 10 // ans is 30
+x = 1
+while 10 > x:
+    # print(x)
+    x+=1
+    if x %2==0:
+        sum+=x
+print(sum)
+    # elif x==11:
+    #     break
+
+'''
+'''
+
+# 7th odd num startiing from 1
+index =1
+x=1
+while 14>x:
+    x+=1
+    
+    if(x%2!=0):
+
+        index+=1
+        print(x ,"is the value")
+        print(index ,"is the index")
+        if (index == 7): 
+            print(x, "no is present at 7th odd position")
+'''
+'''
+
+#factorial of 5
+i=0
+fact=1
+x=5
+while x> i:
+    i+= 1
+    fact*=i
+    print(fact)
+'''
+
